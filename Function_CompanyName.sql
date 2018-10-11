d31e0bda6f63146121bf96c89bb71c7dc1f98ce9
Sifat-Ahmed <sifat.69@live.com>
2018-10-11 22:31:45 +0600

    New Function added

---
+++ Function_CompanyName.sql
@@ -0,0 +1,20 @@
+
+create or replace function getCompanyName(
+ location in varchar)
+	return varchar
+	is
+	NameofCompany varchar;
+
+BEGIN
+	
+	select company_name into NameofCompany from authorized_staff inner join customer on authorized_staff.address = 
+	customer.location where authorized_staff.address = location;
+	return NameofCompany;
+
+EXCEPTION
+	When no_data_found then
+		DBMS_OUTPUT.PUT_LINE('No Data Found');
+	When others then
+		DBMS_OUTPUT.PUT_LINE('Something wrong happened');
+END;
+/
